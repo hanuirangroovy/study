208e39ddd871715f39d12fc2176080a24663118e
haru <jiaiha@naver.com>
2021-09-13 23:28:29 +0900

    210913

--- 210913/5603.py
+++ 210913/5603.py
@@ -5,14 +5,12 @@ for tc in range(int(input())):
     n = int(input())
     lst = [int(input()) for _ in range(n)]
     sum_lst = 0
-    for i in lst:
-        sum_lst += i
-        avg_lst = sum_lst / len(lst)
-        result_lst = []
-        for j in range(n):
+    sum_lst = sum(lst)
+    avg_lst = sum_lst / len(lst)
+    result_lst = []
+    for j in range(n):
+        if lst[j] > avg_lst:
             result_lst.append(abs(lst[j] - avg_lst))
-            result_sum = 0
-            for k in range(len(result_lst)):
-                result_sum += result_lst[k]
-                result = round(result_sum/2)
-print('#{} {}'.format(tc+1, result))
+            result_sum = sum(result_lst)
+            result = round(result_sum)
+    print('#{} {}'.format(tc+1, result))
